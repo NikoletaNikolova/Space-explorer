ba80a7423ec563c99ce5cedbf59f6c0c2522a0ee
unknown <vniko@abv.bg>
2017-10-29 18:10:57 +0200

    Initial demo projects included

---
+++ First Project for Github/SaveMySkin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveMySkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.Write("{0," + i + "}", "*");
+                for (int j = 1; j <= 7 - i; j++)
+                    Console.Write("*");
+
+                {
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
